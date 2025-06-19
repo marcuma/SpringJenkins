b8bf9b2611c55db4f1c78279408ba14b0c4c8c07
marcuma <marcuma@gmail.com>
2025-06-18 22:55:27 -0400

    Added news controller

--- src/test/java/com/simplilearn/springjenkins/springjenkins/NewsControllerTest.java
+++ src/test/java/com/simplilearn/springjenkins/springjenkins/NewsControllerTest.java
@@ -1,24 +1,24 @@
 package com.simplilearn.springjenkins.springjenkins;
 
 import org.junit.jupiter.api.Test;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(HelloController.class)
-class HelloControllerTest {
+@WebMvcTest(NewsController.class)
+class NewsControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    void testHelloWorldEndpoint() throws Exception {
-        mockMvc.perform(get("/hello"))
+    void testGetLatestBusinessNewsHeadline() throws Exception {
+        mockMvc.perform(get("/news/headline"))
                 .andExpect(status().isOk())
-                .andExpect(content().string("Hello World!"));
+                .andExpect(content().string("Starlight, star bright: Coca-Cola introduces a 1st-of-its-kind flavor, inspired by space - KSL.com"));
     }
 }

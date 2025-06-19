0361ddb8b681aeb6a7c854406f30c05db47442c6
marcuma <marcuma@gmail.com>
2025-06-18 22:48:10 -0400

    Added news controller

--- src/test/java/com/simplilearn/springjenkins/springjenkins/HelloControllerTest.java
+++ src/test/java/com/simplilearn/springjenkins/springjenkins/HelloControllerTest.java
@@ -3,7 +3,6 @@ package com.simplilearn.springjenkins.springjenkins;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
